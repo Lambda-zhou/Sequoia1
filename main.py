b385c7eb877ff069f97e3fd9c9360ed6cc1f4302
Lambda-zhou <68288225+Lambda-zhou@users.noreply.github.com>
2024-11-16 18:41:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,25 @@ def job():
     if utils.is_weekday():
         work_flow.prepare()
 
+# 创建一个日志记录器
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-logging.basicConfig(format='%(asctime)s %(message)s', filename='sequoia.log')
-logging.getLogger().setLevel(logging.INFO)
-settings.init()
+# 如果有已有的处理器，先移除它们
+if logger.hasHandlers():
+    logger.handlers.clear()
+
+# Add a StreamHandler to output to the console
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger().addHandler(console)
+
+log_file_handler = logging.FileHandler('console_output.log', mode='a')
+log_file_handler.setLevel(logging.INFO)
+log_file_handler.setFormatter(formatter)
+logging.getLogger().addHandler(log_file_handler)
 
 if settings.config['cron']:
     EXEC_TIME = "15:15"

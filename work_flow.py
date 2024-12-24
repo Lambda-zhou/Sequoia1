2a3ae0bd56d16698e30b12000bfeff3a8302076b
Lambda-zhou <68288225+Lambda-zhou@users.noreply.github.com>
2024-12-24 12:04:40 +0800

    Update work_flow.py

--- work_flow.py
+++ work_flow.py
@@ -21,7 +21,7 @@ import datetime
 def prepare():
     logging.info("************************ process start ***************************************")
     all_data = ak.stock_zh_a_spot_em()
-    subset = all_data[['代码', '名称']]
+    subset = all_data[['代码', '名称','最新价']]
     stocks = [tuple(x) for x in subset.values]
     statistics(all_data, stocks)
 
